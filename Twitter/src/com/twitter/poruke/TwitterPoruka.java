13a037aa76782e83a04413cd8e236038cf397de9
lukadjnovakovic <lukadjnovakovic@gmail.com>
2016-03-26 17:13:18 +0100

    napisana JavaDoc za klasu TwitterPoruka,njene atributr i metode.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,15 +1,34 @@
 package com.twitter.poruke;
 
+/**
+ * Klasa TwitterPoruka opisuje poruku i njenog korisnika
+ * @author LukaNovakovic
+ *
+ */
 public class TwitterPoruka {
 	
+	/**
+	 * Ime korisnika
+	 */
 	private String korisnik;
+	
+	/**
+	 * poruka koju je korisnik uneo
+	 */
 	private String poruka;
 	
-	
+	/**
+	 * Metoda koja vraca ime korisnika neke poruke
+	 * @return korisnik
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
 	
+	/**
+	 * metoda koja postavlja ime nekog korisnika
+	 * @param korisnik
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik==null || korisnik == "")
 			throw new RuntimeException(
@@ -17,11 +36,18 @@ public class TwitterPoruka {
 		this.korisnik = korisnik;
 	}
 	
+	/**
+	 * metoda koja vraca sadrzaj poruke
+	 * @return poruka
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
 	
-	
+	/**
+	 * metoda koja postavlja tekst neke poruke
+	 * @param poruka
+	 */
 	public void setPoruka(String poruka) {
 		if (poruka==null || this.poruka.length()>140)
 			throw new RuntimeException(
@@ -29,6 +55,10 @@ public class TwitterPoruka {
 		this.poruka = poruka;
 	}
 	
+	/**
+	 * metoda koja vraca korisnika i njegovu poruku kao string
+	 * @return korisnik + poruka
+	 */
 	public String toString(){
 		return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
 	}
